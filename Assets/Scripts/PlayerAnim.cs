11d3211e9aa6056be8a34085c71f81280fa5cd6e
Resipate <105903976+Resipate@users.noreply.github.com>
2025-02-23 04:01:40 +1100

    added helicopter animation

--- Assets/Scripts/PlayerAnim.cs
+++ Assets/Scripts/PlayerAnim.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
+    private float currState = 0.5f;
+    [SerializeField] private float timeSinceChange;
+    [SerializeField] private float changeRate;
+    [SerializeField] private Sprite[] states;
+    [SerializeField] private SpriteRenderer sR;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class PlayerAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timeSinceChange < changeRate){ timeSinceChange += Time.deltaTime; }
+        else{
+            currState *= -1;
+            sR.sprite = states[(int)(currState + 0.5f)];
+            timeSinceChange = 0;
+        }
     }
 }
