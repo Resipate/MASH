5de646af9eed2bb66cc161a1e3202d9c13344d81
Resipate <105903976+Resipate@users.noreply.github.com>
2025-02-23 10:34:06 +1100

    added Hospital UI

--- Assets/Scripts/HospitalBehav.cs
+++ Assets/Scripts/HospitalBehav.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HospitalBehav : MonoBehaviour
 {
+    [SerializeField] private Sprite[] states;
+    [SerializeField] private SpriteRenderer sR;
     [SerializeField] private IntUpdater iU;
     [SerializeField] private int score;
     private void OnTriggerEnter2D(Collider2D other) {
@@ -13,6 +16,8 @@ public class HospitalBehav : MonoBehaviour
                 while(result != -1){
                     result = iU.removeSoldier();
                     if(result != -1){ score += 1; }
+                    sR.sprite = states[score];
+                    if(score >= 9){ SceneManager.LoadScene("End"); }
                 }
             }
         }
